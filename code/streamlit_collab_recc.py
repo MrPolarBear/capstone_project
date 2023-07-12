092ef1d185509df70c11c25d8c4cfd13d9e6b532
Sam Song <samueld.song@gmail.com>
2023-07-12 16:49:13 -0500

    wow woo wee

---
+++ code/streamlit_collab_recc.py
@@ -0,0 +1,110 @@
+import streamlit as st
+import pandas as pd
+from surprise import Dataset, Reader, SVD
+
+
+#files to use
+df_main = pd.read_csv("../data/df_prof_rev_anime_clean.csv")
+df_anime = pd.read_csv("../data/df_anime_export.csv")
+
+df_touse = df_main[["profile", "uid", "review_score"]]
+df_anime_touse = df_anime[["uid", "title"]]
+
+##################################################
+# Show Search before using (need EXACT title)
+##################################################
+#take input
+title = st.text_input('Type in show title')
+#display
+st.write("The show you're searching for is: ", title)
+
+# Filter the DataFrame based on partial match
+filtered_shows = df_anime[df_anime['title'].str.contains(title, case=False)]
+filtered_shows = filtered_shows.sort_values(by='popularity', ascending=True)
+
+# Display the filtered shows with 'title' and 'popularity' columns
+st.write("Filtered Shows:")
+st.write(filtered_shows[['title', 'uid','popularity','episodes']])
+
+###############################
+#Personal Ratings
+###############################
+new_rows = [
+    {'profile': 'DargeLildo', 'uid': '2,001', 'review_score': '9'},  #Gurren Lagann
+    {'profile': 'DargeLildo', 'uid': '30', 'review_score': '10'},   #NGEv
+    {'profile': 'DargeLildo', 'uid': '11,061', 'review_score': '10'},    #hxh
+    {'profile': 'DargeLildo', 'uid': '226', 'review_score': '1'},  #elfen
+    {'profile': 'DargeLildo', 'uid': '1,535', 'review_score': '7'},  #deathnote
+    {'profile': 'DargeLildo', 'uid': '918', 'review_score': '5'},    #gintama
+    {'profile': 'DargeLildo', 'uid': '28,977', 'review_score': '5'}    #gintama*
+]
+
+df_touse = pd.concat([df_touse, pd.DataFrame(new_rows)], ignore_index=True)
+
+#############################
+#begin collab recc
+#############################
+df = Dataset.load_from_df(df_touse, Reader(rating_scale=(1, 10)))
+trainset = df.build_full_trainset()
+model = SVD()
+model.fit(trainset)
+
+# Take user input
+user_type = st.radio("Are you a new user or a returning user?", ("New User", "Returning User"))
+st.write("IN THE NEW USER, A RETURNING USER MAY ALSO UES THIS OPTION TO PUT IN A NEW REVIEW AND GET RECOMMENDATIONS BASED ON THAT")
+
+if user_type == "New User":
+    # New user scenario
+    user_id = st.text_input("Enter your user ID")
+    show_id = st.text_input("Enter the show ID")
+    rating = st.slider("Rate the show (1-10)", 1, 10)
+    
+    # Update the training dataset with the user's rating
+    new_row = pd.DataFrame({'profile': [user_id], 'uid': [show_id], 'review_score': [rating]})
+    df_touse_updated = pd.concat([df_touse, new_row], ignore_index=True)
+
+    # Update the model with the updated training dataset
+    df_updated = Dataset.load_from_df(df_touse_updated, Reader(rating_scale=(1, 10)))
+    trainset_updated = df_updated.build_full_trainset()
+    model_updated = SVD()
+    model_updated.fit(trainset_updated)
+
+    # Predict the rating for the show
+    predicted_rating = model_updated.predict(user_id, show_id, r_ui=rating)
+
+    # Get the top rated shows for the user
+    all_anime = df_touse_updated['uid'].unique()
+    watched = df_touse_updated[df_touse_updated['profile'] == user_id].uid
+    not_watched = [anime for anime in all_anime if anime not in watched]
+
+    score = [model_updated.predict(user_id, anime_id).est for anime_id in not_watched]
+    df_pred = pd.DataFrame({'uid': not_watched, 'pred_score': score})
+    df_pred_real = df_pred.sort_values('pred_score', ascending=False).head(10)
+    recommended_shows = df_pred_real.merge(df_anime_touse, how='left', on='uid')
+
+    # Display the predicted rating and recommended shows
+    #st.write("Predicted Rating:")
+    #st.write(predicted_rating.est)
+    st.write("Recommended Shows:")
+    st.write(recommended_shows[['title', 'pred_score']])
+
+elif user_type == "Returning User":
+    # Returning user scenario
+    user_id = st.text_input("Enter your user ID")
+
+    if user_id not in df_touse['profile'].unique():
+        st.error("User ID does not exist. Please enter a valid user ID.")
+    else:
+        # Get the top rated shows for the user
+        all_anime = df_touse['uid'].unique()
+        watched = df_touse[df_touse['profile'] == user_id].uid
+        not_watched = [anime for anime in all_anime if anime not in watched]
+
+        score = [model.predict(user_id, anime_id).est for anime_id in not_watched]
+        df_pred = pd.DataFrame({'uid': not_watched, 'pred_score': score})
+        df_pred_real = df_pred.sort_values('pred_score', ascending=False).head(10)
+        recommended_shows = df_pred_real.merge(df_anime_touse, how='left', on='uid')
+
+        # Display the recommended shows
+        st.write("Recommended Shows:")
+        st.write(recommended_shows[['title', 'pred_score', 'uid']])
---
+++ code/streamlit_content_recc.py
@@ -0,0 +1,164 @@
+import streamlit as st
+import numpy as np 
+import os 
+import pandas as pd 
+import re
+import string
+import seaborn as sns
+from rake_nltk import Rake
+
+import sklearn
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+from sklearn.metrics.pairwise import cosine_similarity
+
+from scipy.sparse import csr_matrix, hstack
+from scipy.sparse import vstack
+
+from fuzzywuzzy import fuzz
+
+
+
+df_anime_nomovie = pd.read_csv("../data/df_anime_nomovie.csv")
+df_anime_all = pd.read_csv("../data/df_anime_all_cleaned.csv")
+
+df_anime_nomovie.dropna(inplace = True)
+df_anime_all.dropna(inplace = True)
+
+########################################################
+#Functions to use
+########################################################
+# Regular recommender, returns similar titles
+def recommend_reg(show_title, n_recom, vectorized_bag_of_words, title, df):
+    similarity_matrix = cosine_similarity(vectorized_bag_of_words, vectorized_bag_of_words[list(np.where(title == show_title)[0]), :])
+    similarity_dataframe = pd.DataFrame(similarity_matrix)
+    similarity_dataframe.index = title
+    similarity_dataframe = similarity_dataframe.iloc[:, 0]
+
+    # Calculate the popularity weights
+    max_popularity = df['popularity_normalized'].max()
+    popularity_weights = (max_popularity - df['popularity_normalized']) / max_popularity
+
+    # Multiply the similarity scores by popularity weights
+    weighted_similarity = similarity_dataframe.values * popularity_weights.values
+
+    similarity_dataframe = pd.Series(weighted_similarity, index=similarity_dataframe.index)
+    similarity_dataframe = similarity_dataframe.sort_values(ascending=False)
+
+    # Exclude the show_title from the recommended titles
+    recommended_titles = list(similarity_dataframe.index)
+    recommended_titles = [title for title in recommended_titles if title != show_title]
+
+    return recommended_titles[:n_recom]
+# Considers ALL titles similar to input + popularity
+def recommend_all_sim(show_title, n_recom, vectorized_bag_of_words, title, df):
+    show_indices = np.where(title == show_title)[0]
+    if len(show_indices) == 0:
+        print(f"Show title '{show_title}' not found in the DataFrame.")
+        return []  # Return an empty list if the show title is not found
+
+    similarity_matrix = cosine_similarity(vectorized_bag_of_words, vectorized_bag_of_words[list(np.where(title == show_title)[0]), :])
+    similarity_dataframe = pd.DataFrame(similarity_matrix)
+    similarity_dataframe.index = title
+
+    similarity_dataframe = similarity_dataframe.iloc[:, [0]]
+
+    # Calculate the popularity weights
+    max_popularity = df['popularity_normalized'].max()
+    
+    popularity_weights = (1 - df['popularity_normalized']) / max_popularity
+
+    #print(popularity_weights)
+
+    # Multiply the similarity scores by popularity weights
+    similarity_dataframe *= popularity_weights
+
+    similarity_dataframe = similarity_dataframe.sort_values(by = 0, ascending=False)
+    similarity_dataframe = similarity_dataframe.drop_duplicates()
+
+    # Find similar titles using fuzzy matching
+    similar_titles = []
+    for title in similarity_dataframe.index:
+        if fuzz.partial_ratio(show_title, title) >= 30:  # Set a threshold for similarity
+            if show_title not in title:
+                similar_titles.append(title)
+
+    return similar_titles[:n_recom]
+# Vectorizer of genre and words
+def vectorize_genre_and_words(genre_column, synopsis, genre_weight=2.0, words_weight=1.0):
+    #extracting keywords for recommender
+    rake = Rake()
+    words = []
+    for plot in synopsis:
+        rake.extract_keywords_from_text(str(plot))
+        keywords_i = rake.get_ranked_phrases()
+        keywords_i_string = ""
+        for keyword in keywords_i:
+            keywords_i_string = keywords_i_string + " " + keyword
+        words.append(keywords_i_string)
+    #temp_df['words'] = words
+    # Ended up not needing above
+    
+    #Adjust weight as needed
+    # Combine genre and words into a single column
+    #combined_column = genre_column + words
+    # This ^ ultimately ended up not being used, will leave in for legacy
+
+    # Create a TF-IDF vectorizer
+    vectorizer_words = TfidfVectorizer()
+    vectorized_words = vectorizer_words.fit_transform(words)
+                                                    #try temp_df['words'] if words dont work
+
+    # Create a TF-IDF vectorizer
+    vectorizer_genre = TfidfVectorizer()
+    vectorized_genre = vectorizer_genre.fit_transform(genre_column)
+
+    # Get the number of genres
+    num_genre_features = vectorized_genre.shape[1]
+
+    # Apply the weights to the genre and words vectors
+    weighted_vectorized_genre = vectorized_genre.multiply(genre_weight)
+    weighted_vectorized_words = vectorized_words.multiply(words_weight)
+
+    # Combine the genre and words vectors
+    vectorized_combined = hstack([weighted_vectorized_genre, weighted_vectorized_words])
+
+    # Convert to csr_matrix
+    vectorized_combined = csr_matrix(vectorized_combined)
+
+    # Convert to array
+    vectorized_combined = vectorized_combined.toarray()
+
+    return vectorized_combined
+
+
+#Process before running rec:
+nomovie_title = df_anime_nomovie['title']
+vectorized_no_movie = vectorize_genre_and_words(df_anime_nomovie['genre'], df_anime_nomovie['synopsis_processed'], genre_weight=1.5, words_weight=0.9)
+
+##################################################
+# Show Search before using (need EXACT title)
+##################################################
+#take input
+title = st.text_input('Type in show title')
+#display
+st.write("The show you're searching for is: ", title)
+
+# Filter the DataFrame based on partial match
+filtered_shows = df_anime_all[df_anime_all['title'].str.contains(title, case=False)]
+filtered_shows = filtered_shows.sort_values(by='popularity', ascending=True)
+
+# Display the filtered shows with 'title' and 'popularity' columns
+st.write("Filtered Shows:")
+st.write(filtered_shows[['title', 'uid','popularity','episodes']])
+
+###################################
+#Content Recc input and use below
+###################################
+content_recc_input = st.text_input("For content recc, copy and paste in from above which show you'd like a recommendation on based on: ")
+st.write("The list below is our recommendation for the show you searched for: ", content_recc_input)
+st.write(recommend_all_sim(content_recc_input,25, vectorized_no_movie, nomovie_title, df_anime_nomovie))
+
+
+#st.write(recommend_all_sim("Fullmetal Alchemist",10, vectorized_no_movie, nomovie_title, df_anime_nomovie))
